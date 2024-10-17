85137012aea38b9c26d9f6dfd08c082b49de5ee8
Collin Olson <newcolson21@gmail.com>
2024-10-17 14:39:59 -0500

    Deleted models, changed app.py

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,10 @@
 from flask import Flask, request, jsonify
 from scraper import scraper
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from models import User
+
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
-
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    'mysql+pymysql://krelljc3490:L5BGBWPC@wayne.cs.uwec.edu:3306/CS485group3'
-)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-new_user = User(username='johndoe', email='johndoe@example.com')
-db.session.add(new_user)
-db.session.commit()
 
 @app.route('/search')
 def search():
