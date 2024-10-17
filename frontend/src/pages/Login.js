cf1a3c01a9a18c72261635e0c71a3c2f0f73e534
Garrett <gj4syth@gmail.com>
2024-10-17 10:13:33 -0500

    Login Page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,9 +1,16 @@
 import React from "react";
+import {Link} from 'react-router-dom';
+import "../styles/Login.css";
 
 const Login = () => {
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="login">
+            <h1 className='loginText'>Login</h1>
+            <h3 className='loginText'>New to Save-A-Tron? Sign up now</h3>
+            <input type="text" className='email' placeholder="Email Address"></input>
+            <input type="password" className='password' placeholder="Password (8+ characters)"></input>
+            <Link to="/forgotPassword" className='forgot'>Forgot Password?</Link>
+            <button type="button" className='button'>Login</button>
         </div>
     );
 };
