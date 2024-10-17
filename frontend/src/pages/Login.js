17800ed015a642c291f5c87c3086f06ebbeb572e
Garrett <gj4syth@gmail.com>
2024-10-17 13:58:27 -0500

    login page text change

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -6,7 +6,6 @@ const Login = () => {
     return (
         <div className="login">
             <h1 className='loginTextHeader'>Login</h1>
-            <h3 className='loginText'>New to Save-A-Tron? Sign up now</h3>
             <input type="text" className='email' placeholder="Email Address"></input>
             <input type="password" className='password' placeholder="Password (8+ characters)"></input>
             <Link to="/forgot" className='forgot'>Forgot Password?</Link>
