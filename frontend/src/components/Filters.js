0740a6de7c4cfe1a616b017cc20497d626e744c3
Olaoluwa Adegoke <laoluadegoke@icloud.com>
2024-10-20 16:25:37 -0500

    Category search functionality
    Updated the search to work with the selected department. The filters list is automatically updated based on the list of brands returned

--- frontend/src/components/Filters.js
+++ frontend/src/components/Filters.js
@@ -7,53 +7,37 @@ const Filters = ({filterList}) => {
   const [showMore, setShowMore] = useState(false);
 
   // Toggle the visibility of the extra filters
-  const toggleShowMore = (category) => {
-    setShowMore((prevState) => ({
-        ...prevState, // Keep previous state values for other categories
-        [category]: !prevState[category], // Toggle the value for the current category
-    }));
+  const toggleShowMore = () => {
+    setShowMore((prevState) => !prevState);
   };
 
+  const visibleFilters = showMore ? filterList : filterList.slice(0, 3); // Show only first 3 filters
 
   return (
     <div className="filters">
-     
-     {/* Loop through each category in the filters list  */}
-     {Object.keys(filterList).map((category) => {
-        const filters = filterList[category]; // Get the list of filters for the current category
-        const visibleFilters = showMore[category]
-          ? filters // If "Show more" is toggled on, show all filters
-          : filters.slice(0, 3); // else, show only the first 3 filters
+      <div className="filter-category">
+        {/* Display the category name */}
+        <div className="filter-category-header">Brands</div>
 
-        return (
-          <div key={category} className="filter-category">
-            {/* Display the category name */}
-            <div className="filter-category-header">{category}</div>
-
-            {/* Show filters for each category*/}
-            {visibleFilters.map((filter, index) => (
-              <div className="filter-item" key={index}>
-                <label>
-                  <input type="checkbox" name={filter.label} value={filter.value} />
-                  {filter.label}
-                </label>
-              </div>
-            ))}
-
-            {/* Show button if there are more than 3 filters */}
-            {filters.length > 3 && (
-              <button
-                onClick={() => toggleShowMore(category)} 
-                className="show-more-btn">
-                {showMore[category] ? "Show less" : "Show more"} {/* Display button label based on toggle state */}
-              </button>
-            )}
+        {/* Show filters for the brands */}
+        {visibleFilters.map((filter, index) => (
+          <div className="filter-item" key={index}>
+            <label>
+              <input type="checkbox" name={filter.label} value={filter.value} />
+              {filter.label}
+            </label>
           </div>
-         
-        );
-      })}
+        ))}
+
+        {filterList.length > 3 && (
+          <button onClick={toggleShowMore} className="show-more-btn">
+            {showMore ? "Show less" : "Show more"}
+          </button>
+        )}
+      </div>
     </div>
   );
+     
 };
 
 export default Filters;
