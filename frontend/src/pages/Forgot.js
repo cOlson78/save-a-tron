d5dc19e263d1f4920dfc4bce44ed689c75e3fb43
forsytgj4194 <gj4syth@gmail.com>
2024-10-30 23:39:13 -0500

    Responsive Design
    Most of it is done, still need to work on the home page and navbar

--- frontend/src/pages/Forgot.js
+++ frontend/src/pages/Forgot.js
@@ -6,7 +6,7 @@ const Forgot = () => {
     return (
         <div className="forgotBox">
             <h1 className='forgotTextHeader'>Forgot Your Password?</h1>
-            <h3 className='forgotText'>Enter your email address and we will send you <br></br> instructions to reset your password</h3>
+            <h3 className='forgotText'>Enter your email address and we will send you instructions to reset your password</h3>
             <input type="text" className='forgotEmail' placeholder="Email Address"></input>
             <Link to="/" className='continue'>Continue</Link>
         </div>
