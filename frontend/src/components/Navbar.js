244b7632a5c619ef64e7bfbb9c830d50fd787386
forsytgj4194 <gj4syth@gmail.com>
2024-11-12 11:39:57 -0600

    responsive design
    work in progress

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { CSSTransition } from 'react-transition-group';
+import { RiArrowDropDownLine } from 'react-icons/ri';
 import {Link} from 'react-router-dom';
 import "../styles/Navbar.css";
+import hamburgIcon from '../assets/Hamburg.png';
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const menuRef = useRef(null);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const handleClickOutside = (event) => {
+        if (menuRef.current && !menuRef.current.contains(event.target)) {
+            setIsOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
     return (
-    <nav className="navbar">  
+    <nav className="navbar">
         <div className="navbar-left nav-item">
             <Link to="/login" className='nav-login'>Login</Link>
-        </div>
+        </div>   
 
         <div className="navbar-center">
             <Link to="/" className='nav-login'>
@@ -20,6 +43,24 @@ const Navbar = () => {
         
         <div className="navbar-right nav-item">
             <Link to="/wishlist" className='nav-wishlist'>Wishlist</Link>
+        </div>
+
+        
+        <div ref={menuRef} className={`dropdown-menu ${isOpen ? 'open' : ''}`}>
+            <button className="dropdown-toggle" onClick={toggleMenu}>
+                Menu
+            </button>
+            <CSSTransition
+                in={isOpen}
+                timeout={300}
+                classNames="dropdown-animation"
+                unmountOnExit
+            >
+                <ul className="dropdown-list"><br></br>
+                    <li><Link to="/login">Login</Link></li>
+                    <li><Link to="/wishlist">Wishlist</Link></li>
+                </ul>
+            </CSSTransition>
         </div>
     </nav>
     );
