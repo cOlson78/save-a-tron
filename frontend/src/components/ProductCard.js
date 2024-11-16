4e21efb333d6de633e8456238aa07224cd33f796
Collin Olson <newcolson21@gmail.com>
2024-11-16 17:20:25 -0600

    Cheaper items filter now only displays cheaper items than was selected.

--- frontend/src/components/ProductCard.js
+++ frontend/src/components/ProductCard.js
@@ -42,7 +42,7 @@ const ProductCard = ({ products }) => {
               Visit Store
             </a>
           </div>
-          <Link to="/cheaper-option" state={{product:product, productTitle:product.title}}>
+          <Link to="/cheaper-option" state={{product:product}}>
             <button className="cheaper-options-button">View Cheaper Options</button> {/* Button for viewing cheaper options */}
           </Link>
         </div>
